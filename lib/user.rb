6aeeed833e39d4487b85575589cd9ae16deacfab
username <wnt925@gmail.com>
2019-05-08 10:19:37 -0600

    Finish user class

--- lib/user.rb
+++ lib/user.rb
@@ -8,4 +8,14 @@ class User
   def learn(joke)
     @jokes << joke
   end
+
+def tell(target, joke)
+  target.jokes << joke
+end
+
+def joke_by_id(id_num)
+  @jokes.select do |joke|
+    joke.id == (id_num)
+  end
+  end
 end
