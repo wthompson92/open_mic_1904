2a3610bfac6c8731f6bca8ec8a7a19d83afa9c95
username <wnt925@gmail.com>
2019-05-08 09:16:30 -0600

    Add joke test

---
+++ test/joke_test.rb
@@ -0,0 +1,36 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/joke'
+
+class JokeTest < Minitest :: Test
+
+def setup
+  @joke = Joke.new(1, "Why did the strawberry cross the road?", "Because his mother was in a jam.")
+
+end
+
+def test_it_exists
+  expected = Joke
+  actual = @joke
+
+  assert_instance_of expected, actual
+end
+
+def test_joke_has_an_id
+  expected = 1
+  actual =  joke.id
+
+end
+
+def test_it_has_a_setup
+
+expected = "Why did the strawberry cross the road?"
+actual = joke.setup
+end
+
+def test_it_has_a_punchline
+  expected =  "Because his mother was in a jam."
+actual = joke.punchline
+end
+
+end
